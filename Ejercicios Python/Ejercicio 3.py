473dfdb22d44c90273187b5fe6d8c4560a479ecb
Moises Charris <moisescharris11@gmail.com>
2025-05-10 11:54:11 -0500

    Se realizaron cambios

--- Ejercicios Python/Ejercicio 3.py
+++ Ejercicios Python/Ejercicio 3.py
@@ -1,10 +1,10 @@
-cantidad = int(input("Ingrese la cantidad de computadores"))
+cantidad = int(input("Ingrese la cantidad de computadores "))
 total_pagar = 3500000 * cantidad
 
 if cantidad < 5:
-    print (total_pagar - (total_pagar * 0.10))
+    print ("Descuento del 10% aplicado ", total_pagar - (total_pagar * 0.10))
 elif cantidad >= 5 and cantidad < 10:
-    print (total_pagar - (total_pagar * 0.20))
+    print ("Descuento del 20% aplicado ", total_pagar - (total_pagar * 0.20))
 else: 
     cantidad >= 10
-    print (total_pagar - (total_pagar * 0.40))
+    print ("Descuento del 40% aplicado ", total_pagar - (total_pagar * 0.40))
