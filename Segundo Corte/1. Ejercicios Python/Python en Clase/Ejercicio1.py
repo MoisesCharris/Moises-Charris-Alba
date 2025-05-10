cdcc51928a6578577550478eb924cae89e7c03bd
Moises Charris <moisescharris11@gmail.com>
2025-05-10 08:31:06 -0500

    cambios

--- Segundo Corte/1. Ejercicios Python/Python en Clase/Ejercicio1.py
+++
@@ -1,8 +0,0 @@
-nh = int(input("Ingrese nh: ")) 
-nm = int(input("Ingrese nm: "))  
-
-ta = nh + nm                   
-ph = nh * 100 / ta             
-pm = nm * 100 / ta             
-
-print(ph, pm)                  
--- Segundo Corte/1. Ejercicios Python/Python en Clase/Ejercicio2.py
+++
@@ -1,5 +0,0 @@
-
-cap = int(input("capital"))
-if (int > 7000):
-    capf = cap + int 
-print (capf)
