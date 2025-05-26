d360bd93afdeb75f55c00cff44aa53605114d1da
Moises Charris <moisescharris11@gmail.com>
2025-05-26 17:00:52 -0500

    agrego

---
+++ Ejercicios Python/Ejercicios 19dia.py
@@ -0,0 +1,10 @@
+
+numero = int(input("ingrese un numero "))
+print (numero ** (1/4))
+
+radio = int(input("ingrese el radio del circulo "))
+print ("el area del circulo es", 3.14 * (radio ** 2))
+
+nombre = str(input("nombre del articulo "))
+costoproduccion = int(input("ingrese el precio "))
+print ("el precio de venta del", nombre, "es de", round(costoproduccion * 2.20 * 1.15))
